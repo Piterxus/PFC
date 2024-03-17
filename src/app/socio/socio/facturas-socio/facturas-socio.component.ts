1506e06c185185a2fd24a5c5f50268f372140feb
Piterxus <piterxus@gmail.com>
2024-03-17 20:40:24 +0100

    chore: Maquetación de mostrar facturas ok

--- src/app/socio/socio/facturas-socio/facturas-socio.component.ts
+++ src/app/socio/socio/facturas-socio/facturas-socio.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-facturas-socio',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./facturas-socio.component.css']
 })
 export class FacturasSocioComponent {
+  constructor(private router: Router) { }
 
+  volver() {
+    this.router.navigate(['/panel']);
+  }
 }
