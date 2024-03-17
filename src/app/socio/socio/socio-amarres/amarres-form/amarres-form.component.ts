03c3955f2d62bd979ffcf6ae500df31c7fc8cfd9
Piterxus <piterxus@gmail.com>
2024-03-17 19:56:51 +0100

    chore: Maquetación de amarres socio finalizada

--- src/app/socio/socio/socio-amarres/amarres-form/amarres-form.component.ts
+++ src/app/socio/socio/socio-amarres/amarres-form/amarres-form.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-amarres-form',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./amarres-form.component.css']
 })
 export class AmarresFormComponent {
+  constructor(private router: Router) { }
 
+  volver() {
+    this.router.navigate(['/amarres-socio']);
+  }
 }
