36b196a109d282dfcf70289dc46a17d213228530
Piterxus <piterxus@gmail.com>
2024-03-17 17:39:50 +0100

    feat: Crear componente de formulario para miembros

---
+++ src/app/socio/socio/socio-miembros/miembros-form/miembros-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-miembros-form',
+  templateUrl: './miembros-form.component.html',
+  styleUrls: ['./miembros-form.component.css']
+})
+export class MiembrosFormComponent {
+
+}
--- src/app/socio/socio/socio.module.ts
+++ src/app/socio/socio/socio.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SocioComponent } from './socio.component';
+import { MiembrosFormComponent } from './socioMiembros/miembros-form/miembros-form.component';
 
 
 
 @NgModule({
   declarations: [
-    SocioComponent
+    SocioComponent,
+    MiembrosFormComponent
   ],
   imports: [
     CommonModule
