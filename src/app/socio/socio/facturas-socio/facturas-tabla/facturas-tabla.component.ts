4216a0cdd469480214a500e56688dc6db5a1f166
piterxus <piterxus@gmail.com>
2024-03-18 18:57:31 +0100

    feat: revisión completada de enlaces de la tabla

--- src/app/socio/socio/facturas-socio/facturas-tabla/facturas-tabla.component.ts
+++ src/app/socio/socio/facturas-socio/facturas-tabla/facturas-tabla.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-facturas-tabla',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./facturas-tabla.component.css']
 })
 export class FacturasTablaComponent {
+  constructor(private router: Router) { } 
 
+  verDetalle() {
+    this.router.navigate(['/facturas/detalle-factura']);
+  }
 }
