694a2e9bea6940958f3b28ed09dd5f411cfaa113
Piterxus <piterxus@gmail.com>
2024-03-22 20:34:24 +0100

    feat: Maquetación completa de las formas de pago

--- src/app/socio/socio/formas-pago/formas-pago.component.ts
+++ src/app/socio/socio/formas-pago/formas-pago.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-formas-pago',
@@ -9,14 +10,17 @@ export class FormasPagoComponent {
   tipoFormulario: string = '';
   mostrarTablas: boolean = true;
 
-  constructor() { }
+  constructor(private router: Router, private route: ActivatedRoute) { }
 
   mostrarFormulario(tipo: string) {
+    console.log('tipo', tipo);
     this.tipoFormulario = tipo;
+    this.router.navigate([], { relativeTo: this.route, queryParams: { tipo: tipo }, queryParamsHandling: 'merge' });
     this.mostrarTablas = false;
   }
 
   volverATablas() {
+    this.router.navigate([], { relativeTo: this.route, queryParams: { tipo: null }, queryParamsHandling: 'merge' });
     this.mostrarTablas = true;
   }
 
